2baa733b90e10d82757525a2031aaacc02653a95
Alex Jamison <business.homeit@gmail.com>
2024-04-18 18:25:52 +0300

    make git stuff

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -2,8 +2,10 @@ function GitCheckout()
     local input = vim.fn.input("Change branch to: ")
     vim.cmd("Git checkout " .. input)
 end
+
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-vim.keymap.set("n", "<leader>ga", ":vertical Git add -p<CR>")
+vim.keymap.set("n", "<leader>ga", ":Git add -A<CR>")
+vim.keymap.set("n", "<leader>gap", ":vertical Git add -p<CR>")
 vim.keymap.set("n", "<leader>gc", ":vertical Git commit<CR>")
 vim.keymap.set("n", "<leader>gp", ":Git push<CR>")
 vim.keymap.set("n", "<leader>gb", ":lua GitCheckout()<CR>")
