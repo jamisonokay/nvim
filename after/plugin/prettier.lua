6eb9aa66a9465f2be50f74e7d0cc181e2a0765a7
Alex Jamison <business.homeit@gmail.com>
2024-12-20 16:24:18 +0300

    update prettier for elixir

--- after/plugin/prettier.lua
+++ after/plugin/prettier.lua
@@ -19,4 +19,11 @@ prettier.setup({
         "go"
     },
 })
-vim.keymap.set("n", "<leader>f", ":Prettier<CR>")
+vim.keymap.set("n", "<leader>f", function ()
+    local file_ext = vim.fn.expand("%:e")
+    if file_ext == "ex" or file_ext == "exs" then
+        vim.cmd("silent! !mix format %")
+    else
+        vim.cmd(":Prettier")
+    end
+end)
